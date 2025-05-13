e95806ccb7ce2ab48df518370b1da0364b0426d5
dineshkhadka9863 <dkhadka1000@gmail.com>
2025-05-13 12:36:55 +0545

    dinesh

--- functions.php
+++ functions.php
@@ -348,7 +348,7 @@ function theme_stylescript(){
     wp_enqueue_style( 'owlcarousel_css', get_template_directory_uri() . '/assets/vendor/owl/owl.carousel.min.css' );
     wp_enqueue_style( 'bootstrap_icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css' );
 	// wp_enqueue_style( 'tailwind_css', get_template_directory_uri() . '/assets/css/src/output.css', array(), '0.2.8' );
-    wp_enqueue_style( 'nationalherbo_css', get_template_directory_uri() . '/assets/css/theme.css', array(), '0.2.8' );
+    wp_enqueue_style( 'nationalherbo_css', get_template_directory_uri() . '/assets/css/theme.css', array(), '0.3.1' );
     // Enqueue jQuery
 // wp_enqueue_script('jquery');
      // Enqueue jQuery from CDN
