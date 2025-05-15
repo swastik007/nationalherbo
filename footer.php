03e4751df55e403ed2c609829ca368eddc805317
Ashish Khadka <aasheeshkhadka@gmail.com>
2025-05-15 12:11:38 +0545

    footer and fonts arranged

--- footer.php
+++ footer.php
@@ -11,42 +11,39 @@
   <div class="container">
     <div class="row g-4">
       <div class="col-md-6 col-lg-3">
-        <h5 class="mb-3 text-warning">Head Office</h5>
+        <h3 class="text-warning">Head Office</h3>
         <p>D2/2 - 2nd Floor, Sorakhutte <br> Kathmandu, Nepal</p>
       </div>
       <div class="col-md-6 col-lg-3">
-        <h5 class="mb-3 text-warning">Sub Office</h5>
+        <h3 class="text-warning">Sub Office</h3>
         <p>Kupondole-2<br>Lalitpur, Nepal</p>
       </div>
       <div class="col-md-6 col-lg-3">
-        <h5 class="mb-3 text-warning">Phone Number</h5>
+        <h3 class="text-warning">Phone Number</h3>
         <p>+91-9829885383</p>
-        <h5 class="mb-3 text-warning">Email Address</h5>
+        <h3 class="text-warning">Email Address</h3>
         <p>info@nationalherbo.com</p>
       </div>
-      <div class="col-md-6 col-lg-3">
-        <h5 class="mb-3 text-white">Locate Us</h5>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.234567890123!2d85.30936!3d27.71866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb190123456789%3A0xabcdef1234567890!2sSorakhutte%2C%20Kathmandu%2C%20Nepal!5e0!3m2!1sen!2snp!4v1715600000000"
-          width="100%"
-          height="200"
-          style="border:0;"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
+      <div class="col-md-3">
+        <h4 class="text-white">Follow Us</h4>
+        <div class="d-flex gap-2">
+          <a href="#" class="text-white fs-5"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" class="text-white fs-5"><i class="fab fa-instagram"></i></a>
+          <a href="#" class="text-white fs-5"><i class="fab fa-linkedin-in"></i></a>
+          <a href="#" class="text-white fs-5"><i class="fab fa-youtube"></i></a>
+        </div>
       </div>
     </div>
 
-    <hr class="my-4 border-light">
+    <hr class="border-light my-1">
 
     <div class="row g-4">
       <div class="col-md-3">
-        <h6 class="text-white mb-3">About Us</h6>
+        <h4 class="text-white">About Us</h4>
         <p>We offer top-quality Ayurvedic & Nutritional Products as a trusted third-party manufacturer across India.</p>
       </div>
       <div class="col-md-3">
-        <h6 class="text-white mb-3">Products</h6>
+        <h4 class="text-white">Products</h4>
         <ul class="list-unstyled">
           <li>› Herbal Syrups & Tonics</li>
           <li>› Capsules & Tablets</li>
@@ -56,7 +53,7 @@
         </ul>
       </div>
       <div class="col-md-3">
-        <h6 class="text-white mb-3">Quick Links</h6>
+        <h4 class="text-white">Quick Links</h4>
         <ul class="list-unstyled">
           <li><a href="#" class="text-white text-decoration-none">Home</a></li>
           <li><a href="#" class="text-white text-decoration-none">About Us</a></li>
@@ -64,14 +61,17 @@
           <li><a href="#" class="text-white text-decoration-none">Contact</a></li>
         </ul>
       </div>
-      <div class="col-md-3">
-        <h6 class="text-white mb-3">Follow Us</h6>
-        <div class="d-flex gap-2">
-          <a href="#" class="text-white fs-5"><i class="fab fa-facebook-f"></i></a>
-          <a href="#" class="text-white fs-5"><i class="fab fa-instagram"></i></a>
-          <a href="#" class="text-white fs-5"><i class="fab fa-linkedin-in"></i></a>
-          <a href="#" class="text-white fs-5"><i class="fab fa-youtube"></i></a>
-        </div>
+      <div class="col-md-6 col-lg-3">
+        <h4 class="text-white">Locate Us</h4>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.234567890123!2d85.30936!3d27.71866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb190123456789%3A0xabcdef1234567890!2sSorakhutte%2C%20Kathmandu%2C%20Nepal!5e0!3m2!1sen!2snp!4v1715600000000"
+          width="100%"
+          height="200"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
       </div>
     </div>
 
